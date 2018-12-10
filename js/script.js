247e75a3de6d6b8deb635d209e7ff39c68b4194d
kelvinadeu <kelvinadeu25@gmail.com>
2018-12-10 15:48:30 +0300

    add to page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function submitAnswers(){
+
+  var q1=document.getElementById().value;
+  var q2=document.getElementById().value;
+  var q3=document.getElementById().value;
+  var q4=document.getElementById().value;
+  var q5=document.getElementById().value;
+}
