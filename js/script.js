fdece7be4b3642c422d422256f6b7a7b1a09c2f5
kelvinadeu <kelvinadeu25@gmail.com>
2018-12-10 18:13:01 +0300

    add to page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-function submitAnswers(){
-
-  var q1=document.getElementById().value;
-  var q2=document.getElementById().value;
-  var q3=document.getElementById().value;
-  var q4=document.getElementById().value;
-  var q5=document.getElementById().value;
+ var sum=0
+function score(){
+var questions=["q1","q2","q3","q4","q5"];
+questions.forEach(function(question){
+  var userValue=$("input:radio[name="+question+"]:checked").val();
+      if( userValue ==="20"){
+        sum+=20;
+      }
+});
 }
+$(document).ready(function(){
+  $("#start").click(function() {
+   $(".form").show();
+ })
+ $("#submit").click(function() {
+     event.preventDefault();
+     $("#score").show();
+     $(".form").hide();
+     score();
+     if(sum <= 60) {
+       $(".name-display").text(`Pull up your socks!`);
+
+     }
+     else {
+       $(".name-display").text(`Congratulations!`);
+
+     }
+     $(".scoreDisplay").text(sum + "/100");
+   })
+
+
+});
